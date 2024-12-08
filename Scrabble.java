4f044b1c38c161330b7031612b338a95045d2351
odedraban <odedraban@post.runi.ac.il>
2024-12-08 20:03:40 +0200

    exercise 2

--- Scrabble.java
+++ Scrabble.java
@@ -104,14 +104,13 @@ public class Scrabble {
 					System.out.println(input + " earned " + wordScore(input) + " points. Score: " + score + " points");
 					System.out.println("");
 				}
-				//// else if ((isWordInDictionary(input)) && (MyString.subsetOf(input, hand) == false)) {
 				else if (!MyString.subsetOf(input, hand)) {
 					System.out.println("Invalid word. Try again.");
-					System.out.println("");
+					////System.out.println("");
 				}		
 				else if (!isWordInDictionary(input)) {
 					System.out.println("No such word in the dictionary. Try again.");
-					System.out.println("");
+					////System.out.println("");
 				}
 			}
 			if (input.equals(".")) break;
